fce8ab5aef222fe106bfcaa188680b3d1516fa27
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-11 11:21:28 +0300

    branch class impllimented

--- Banking/src/com/company/Branch.java
+++ Banking/src/com/company/Branch.java
@@ -3,13 +3,45 @@ package com.company;
 import java.util.ArrayList;
 
 public class Branch {
-   private  ArrayList<Customer> Customers = new ArrayList<Customer>;
 
-    public Branch(ArrayList<Customer> customers) {
-        Customers = customers;
+    private String name;
+    private  ArrayList<Customer> Customers;
+
+    public String getName() {
+        return name;
     }
 
-    public void AddCustomer (){
-        Customers.add(new customer{})
+    public Branch(String name) {
+        this.name = name;
+        this.Customers=new ArrayList<Customer>();
     }
+
+
+
+    public boolean NewCustomer (String customerName, double initialAmount){
+        if(FindCustomer(customerName) == null) {
+            this.Customers.add(new Customer(customerName,initialAmount));
+            return true;
+            }
+        return false;
+        }
+    public boolean AddCustomerTransaction(String customerName,double amount){
+        Customer existingCustomer = FindCustomer(customerName);
+        if(existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+        }
+        return false;
+    }
+    private Customer FindCustomer(String customerName){
+        for(int i=0; i<this.Customers.size();i++){
+            if (this.Customers.get(i).equals(customerName)){
+                return this.Customers.get(i);
+            }
+        }
+        return null;
+    }
+
+
+
 }
+
